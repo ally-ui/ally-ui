5479eb7ce64f8ce3bba3b368c65faacf8c2ec0ce
Bryan Lee <bryanmylee@gmail.com>
2022-08-17 22:23:22 +1000

    org: per-project test config

--- packages/common/focus-trap/vitest.config.js
+++ packages/common/focus-trap/vitest.config.js
@@ -3,6 +3,6 @@ import {defineConfig} from 'vitest/config';
 export default defineConfig({
 	test: {
 		environment: 'jsdom',
-		setupFiles: './vitest.setup.ts',
+		setupFiles: './vitest.setup.js',
 	},
 });
