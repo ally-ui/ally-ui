e2c73a9eda8176a7299c085654e2c28a1bccfbb4
Bryan Lee <bryanmylee@gmail.com>
2022-08-17 17:29:12 +1000

    react(doc): useRunOnce

--- packages/common/react/lib/useRunOnce.tsx
+++ packages/common/react/lib/useRunOnce.tsx
@@ -1,5 +1,10 @@
 import {useState} from 'react';
 
+/**
+ * Run a function once **during render**.
+ * @param runner The function to run
+ * @returns The value returned by `runner`
+ */
 export default function useRunOnce<TReturn>(runner: () => TReturn) {
 	const [value] = useState(runner);
 	return value;
