841485c44df4191683c3895cc51cb4a7ef3d3fad
Bryan Lee <bryanmylee@gmail.com>
2022-08-19 00:31:21 +1000

    svelte-dialog(test-ssr): initial attributes

--- packages/dialog/svelte/lib/__tests__/ssr/__tests__/attributes.test.ts
+++ packages/dialog/svelte/lib/__tests__/ssr/__tests__/attributes.test.ts
@@ -1,19 +1,40 @@
 import {screen} from '@testing-library/dom';
-import {describe, expect, it} from 'vitest';
+import {afterEach, beforeAll, beforeEach, describe, expect, it} from 'vitest';
 import {renderServerTemplate} from '../renderServerTemplate';
 
+let rendered_open: string;
+let rendered_closed: string;
+beforeAll(async () => {
+	rendered_open = await renderServerTemplate('init--open');
+	rendered_closed = await renderServerTemplate('init--closed');
+});
+afterEach(() => {
+	document.body.innerHTML = '';
+});
+
 describe('trigger', () => {
-	it('renders the data state attribute on a closed dialog', async () => {
-		const rendered = await renderServerTemplate('init--closed');
-		document.body.innerHTML = rendered;
+	it('renders the data state attribute with a closed dialog', async () => {
+		document.body.innerHTML = rendered_closed;
 		const trigger = screen.getByTestId('trigger');
 		expect(trigger).toHaveAttribute('data-state', 'closed');
 	});
 
-	it('renders the data state attribute on an open dialog', async () => {
-		const rendered = await renderServerTemplate('init--open');
-		document.body.innerHTML = rendered;
+	it('renders the data state attribute with an open dialog', async () => {
+		document.body.innerHTML = rendered_open;
 		const trigger = screen.getByTestId('trigger');
 		expect(trigger).toHaveAttribute('data-state', 'open');
+	});
+
+	it('renders basic aria attributes', async () => {
+		document.body.innerHTML = rendered_closed;
+		const trigger = screen.getByTestId('trigger');
+		expect(trigger).toHaveAttribute('aria-haspopup', 'dialog');
+	});
+
+	it('has aria-controls that points to content', async () => {
+		document.body.innerHTML = rendered_open;
+		const trigger = screen.getByTestId('trigger');
+		const content = screen.getByTestId('content');
+		expect(trigger).toHaveAttribute('aria-controls', content.id);
 	});
 });
