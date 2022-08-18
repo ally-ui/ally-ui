d97a3aa7a9de347741c2cc2f2e409a755b927a5d
Bryan Lee <bryanmylee@gmail.com>
2022-08-18 10:47:34 +1000

    svelte(feat): add vite ssr-svelte example

---
+++ packages/dialog/ssr-svelte/vite.config.js
@@ -0,0 +1,13 @@
+import {svelte} from '@sveltejs/vite-plugin-svelte';
+import {defineConfig} from 'vite';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+	plugins: [
+		svelte({
+			compilerOptions: {
+				hydratable: true,
+			},
+		}),
+	],
+});
