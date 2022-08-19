9fa3dc1442d025c5732328d1f4df3223161653a4
Bryan Lee <bryanmylee@gmail.com>
2022-08-19 18:41:33 +1000

    test: toHaveFocusWithin matcher

--- packages/dialog/svelte/vitest.setup.ts
+++ packages/dialog/svelte/vitest.setup.ts
@@ -2,3 +2,27 @@ import matchers from '@testing-library/jest-dom/matchers';
 import {expect} from 'vitest';
 
 expect.extend(matchers);
+expect.extend({
+	toHaveFocusWithin(element: unknown) {
+		if (!(element instanceof HTMLElement || element instanceof SVGElement)) {
+			return {
+				pass: false,
+				message: () => `Expected ${element} to be an HTMLElement or SVGElement`,
+			};
+		}
+		const hasFocusWithin = element.contains(
+			element.ownerDocument.activeElement,
+		);
+		if (hasFocusWithin) {
+			return {
+				pass: true,
+				message: () => `Expected ${element} not to have focus within`,
+			};
+		} else {
+			return {
+				pass: false,
+				message: () => `Expected ${element} to have focus within`,
+			};
+		}
+	},
+});
