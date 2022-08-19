2e1dafe3d1bdb323ddd113d0fc108960bdd02483
Bryan Lee <bryanmylee@gmail.com>
2022-08-19 22:57:56 +1000

    react-dialog(test): attributes

---
+++ packages/dialog/react/lib/__tests__/attributes.test.tsx
@@ -0,0 +1,87 @@
+import {
+	cleanup,
+	render,
+	screen,
+	waitForElementToBeRemoved,
+} from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import React from 'react';
+import {Dialog, useDialog} from '../main';
+import {UseDialogOptions} from '../useDialog';
+
+function Attributes(options: UseDialogOptions) {
+	const dialog = useDialog(options);
+	return (
+		<React.StrictMode>
+			<Dialog.Trigger model={dialog} data-testid="trigger">
+				open dialog
+			</Dialog.Trigger>
+			<Dialog.Content model={dialog} data-testid="content">
+				<Dialog.Title model={dialog} data-testid="title">
+					title
+				</Dialog.Title>
+				<Dialog.Description model={dialog} data-testid="description">
+					description
+				</Dialog.Description>
+				<Dialog.Close model={dialog} data-testid="close">
+					close dialog
+				</Dialog.Close>
+			</Dialog.Content>
+		</React.StrictMode>
+	);
+}
+
+afterEach(() => {
+	cleanup();
+});
+
+describe('content', () => {
+	it('renders the data state attribute on an open dialog', () => {
+		render(<Attributes initialOpen />);
+		const content = screen.getByTestId('content');
+		expect(content).toHaveAttribute('data-state', 'open');
+	});
+
+	it('renders basic aria attributes', () => {
+		render(<Attributes initialOpen />);
+		const content = screen.getByTestId('content');
+		expect(content).toHaveAttribute('role', 'dialog');
+		expect(content).toHaveAttribute('aria-modal', 'true');
+	});
+
+	it('renders aria attributes that point to title and description', () => {
+		render(<Attributes initialOpen />);
+		const content = screen.getByTestId('content');
+		const title = screen.getByTestId('title');
+		const description = screen.getByTestId('description');
+		expect(content).toHaveAttribute('aria-labelledby', title.id);
+		expect(content).toHaveAttribute('aria-describedby', description.id);
+	});
+});
+
+describe('trigger', () => {
+	it('renders the data state attribute with a closed dialog', () => {
+		render(<Attributes />);
+		const trigger = screen.getByTestId('trigger');
+		expect(trigger).toHaveAttribute('data-state', 'closed');
+	});
+
+	it('renders the data state attribute with an open dialog', () => {
+		render(<Attributes initialOpen />);
+		const trigger = screen.getByTestId('trigger');
+		expect(trigger).toHaveAttribute('data-state', 'open');
+	});
+
+	it('renders basic aria attributes', () => {
+		render(<Attributes />);
+		const trigger = screen.getByTestId('trigger');
+		expect(trigger).toHaveAttribute('aria-haspopup', 'dialog');
+	});
+
+	it('renders aria-controls that points to content', () => {
+		render(<Attributes initialOpen />);
+		const trigger = screen.getByTestId('trigger');
+		const content = screen.getByTestId('content');
+		expect(trigger).toHaveAttribute('aria-controls', content.id);
+	});
+});
---
+++ packages/dialog/react/lib/__tests__/toggle/trigger-data-state-attribute.test.tsx
@@ -0,0 +1,14 @@
+import {render, screen} from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import Toggle from './Toggle';
+
+it('updates the data state attribute when the dialog opens and closes', async () => {
+	const user = userEvent.setup();
+	render(<Toggle initialOpen />);
+	const trigger = await screen.findByTestId('trigger');
+	await user.click(await screen.findByTestId('close'));
+	expect(trigger).toHaveAttribute('data-state', 'closed');
+	await user.click(trigger);
+	await screen.findByTestId('title');
+	expect(trigger).toHaveAttribute('data-state', 'open');
+});
