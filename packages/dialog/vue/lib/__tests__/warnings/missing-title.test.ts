3962944877992f9eb1871c66cdc2b1aabd3c1cc7
Bryan Lee <bryanmylee@gmail.com>
2022-08-28 23:02:24 +1000

    core-dialog(nit): more specific warning

--- packages/dialog/vue/lib/__tests__/warnings/missing-title.test.ts
+++ packages/dialog/vue/lib/__tests__/warnings/missing-title.test.ts
@@ -1,3 +1,4 @@
+import {DialogModel} from '@ally-ui/core-dialog';
 import {cleanup, render, screen} from '@testing-library/vue';
 import MissingTitle from './missing-title.test.vue';
 
@@ -9,7 +10,5 @@ it('warns the user if the title component is missing', async () => {
 	const warnSpy = vi.spyOn(console, 'warn');
 	render(MissingTitle);
 	await screen.findByTestId('content');
-	expect(warnSpy).toHaveBeenCalledWith(
-		'Dialogs should contain a title component for accessibility reasons',
-	);
+	expect(warnSpy).toHaveBeenCalledWith(DialogModel.MISSING_TITLE_WARNING);
 });
