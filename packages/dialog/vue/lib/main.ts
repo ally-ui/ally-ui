af04c93e3f747394632ec99d76426cd92300d421
Bryan Lee <bryanmylee@gmail.com>
2022-08-30 15:14:27 +1000

    vue-dialog(build): export component prop types

--- packages/dialog/vue/lib/main.ts
+++ packages/dialog/vue/lib/main.ts
@@ -2,7 +2,7 @@ export * from '@ally-ui/core-dialog';
 export {default as DialogClose} from './DialogClose.vue';
 export {default as DialogContent} from './DialogContent.vue';
 export {default as DialogDescription} from './DialogDescription.vue';
-export {default as DialogRoot} from './DialogRoot.vue';
+export {default as DialogRoot, type DialogRootProps} from './DialogRoot.vue';
 export {default as DialogTitle} from './DialogTitle.vue';
 export {default as DialogTrigger} from './DialogTrigger.vue';
 
