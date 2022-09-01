583ce0b47821b500bccc8249a190a43a86825533
Bryan Lee <bryanmylee@gmail.com>
2022-09-01 12:54:02 +1000

    solid(feat): Slot component

--- packages/common/solid/lib/main.ts
+++ packages/common/solid/lib/main.ts
@@ -1,4 +1,6 @@
-export {combinedRef} from './combinedRef';
-export {createBindRef, createDelayedBindRef} from './createBindRef';
-export {createSyncedOption} from './createSyncedOption';
-export {forwardEvent} from './forwardEvent';
+export * from './combinedRef';
+export * from './createBindRef';
+export * from './createSyncedOption';
+export * from './forwardEvent';
+export * from './Slot';
+export * from './types';
