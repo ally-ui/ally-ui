48d30d99621d88e77f3546413ee26bc92de87e0c
Bryan Lee <bryanmylee@gmail.com>
2022-09-01 10:49:28 +1000

    svelte(feat): slots helper types

--- packages/common/svelte/lib/main.ts
+++ packages/common/svelte/lib/main.ts
@@ -3,4 +3,5 @@ export * from './createContext';
 export * from './createEventForwarder';
 export * from './createRefAction';
 export * from './createSyncedOption';
+export * from './slots';
 export * from './store';
