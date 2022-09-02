59a8df67a91cf54368e41511bfc8a7013101b651
Bryan Lee <bryanmylee@gmail.com>
2022-09-02 22:12:53 +1000

    vue-dialog(fix): use state type

--- packages/dialog/vue/lib/context.ts
+++ packages/dialog/vue/lib/context.ts
@@ -1,13 +1,9 @@
-import type {
-	DialogRootModel,
-	DialogRootModelOptions,
-	DialogRootModelState,
-} from '@ally-ui/core-dialog';
+import type {DialogRootModel, DialogRootModelState} from '@ally-ui/core-dialog';
 import type {InjectionKey, Ref} from 'vue';
 
 export const DIALOG_ROOT_MODEL = Symbol(
 	'DIALOG_ROOT_MODEL',
 ) as InjectionKey<DialogRootModel>;
 export const DIALOG_ROOT_STATE = Symbol('DIALOG_ROOT_STATE') as InjectionKey<
-	Ref<DialogRootModelOptions & DialogRootModelState>
+	Ref<DialogRootModelState>
 >;
