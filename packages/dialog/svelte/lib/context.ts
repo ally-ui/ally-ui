3c65ebbcbb30fbb6bc59242f73e54adc7ab857d2
Bryan Lee <bryanmylee@gmail.com>
2022-09-02 22:07:55 +1000

    svelte-dialog(fix): use state type

--- packages/dialog/svelte/lib/context.ts
+++ packages/dialog/svelte/lib/context.ts
@@ -1,14 +1,8 @@
-import type {
-	DialogRootModel,
-	DialogRootModelOptions,
-	DialogRootModelState,
-} from '@ally-ui/core-dialog';
+import type {DialogRootModel, DialogRootModelState} from '@ally-ui/core-dialog';
 import {createContext} from '@ally-ui/svelte';
 import type {Readable} from 'svelte/store';
 
 export const [getDialogRootModel, setDialogRootModel] =
 	createContext<DialogRootModel>('DIALOG_ROOT_MODEL');
 export const [getDialogRootState, setDialogRootState] =
-	createContext<Readable<DialogRootModelOptions & DialogRootModelState>>(
-		'DIALOG_ROOT_STATE',
-	);
+	createContext<Readable<DialogRootModelState>>('DIALOG_ROOT_STATE');
