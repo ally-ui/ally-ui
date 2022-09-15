ae2911fa834028d42fd81aad2cc6676d344723f0
Bryan Lee <bryanmylee@gmail.com>
2022-09-15 23:40:22 +1000

    website(build): plugins

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -1,14 +1,22 @@
-import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
 import preact from '@astrojs/preact';
 import react from '@astrojs/react';
+import solid from '@astrojs/solid-js';
+import svelte from '@astrojs/svelte';
+import tailwind from '@astrojs/tailwind';
+import vue from '@astrojs/vue';
+import {defineConfig} from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [
-		// Enable Preact to support Preact JSX components.
 		preact(),
-		// Enable React for the Algolia search component.
 		react(),
+		mdx(),
+		svelte(),
+		solid(),
+		tailwind(),
+		vue(),
 	],
 	site: `http://astro.build`,
 });
