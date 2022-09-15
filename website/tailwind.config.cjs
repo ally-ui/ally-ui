42dac726b0fed7e7e025b36956bae7267c4ad267
Bryan Lee <bryanmylee@gmail.com>
2022-09-16 00:04:43 +1000

    website(nit): rem breakpoints

--- website/tailwind.config.cjs
+++ website/tailwind.config.cjs
@@ -2,6 +2,11 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+		screens: {
+			sm: '37.75rem',
+			md: '50rem',
+			lg: '72rem',
+		},
 		extend: {},
 	},
 	plugins: [],
