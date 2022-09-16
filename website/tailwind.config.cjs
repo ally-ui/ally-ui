c651ca2a75ce261f66e79f57983d268d5ad70ed8
Bryan Lee <bryanmylee@gmail.com>
2022-09-16 19:50:57 +1000

    website(nit): tailwind class dark mode

--- website/tailwind.config.cjs
+++ website/tailwind.config.cjs
@@ -2,6 +2,7 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	darkMode: 'class',
 	theme: {
 		screens: {
 			sm: '37.75rem',
