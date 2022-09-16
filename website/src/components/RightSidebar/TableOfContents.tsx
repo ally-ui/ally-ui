38af7369f09f73c6f2613e026be8355e61bbc701
Bryan Lee <bryanmylee@gmail.com>
2022-09-16 22:42:19 +1000

    website(style): match ride sidebar to left

--- website/src/components/RightSidebar/TableOfContents.tsx
+++ website/src/components/RightSidebar/TableOfContents.tsx
@@ -9,27 +9,31 @@ const TableOfContents: FunctionalComponent<{headings: MarkdownHeading[]}> = ({
 	const [activeId] = useState<string>('overview');
 	return (
 		<>
-			<h2 className="mb-2 text-xl font-bold">On this page</h2>
+			<h2 className="mb-2 text-lg font-bold">On this page</h2>
 			<ul>
-				<li
-					className={cx(
-						'hover:text-accent focus:text-accent border-l-2 border-shade-200 py-1 px-2',
-						{
-							'text-accent border-accent': activeId === 'overview',
-						},
-					)}
-				>
-					<a href="#overview">Overview</a>
+				<li>
+					<a
+						href="#overview"
+						className={cx(
+							'block px-4 py-2 font-medium border-l-2 hover:text-accent focus:text-accent border-shade-200 ring-inset',
+							{
+								'text-accent border-accent': activeId === 'overview',
+							},
+						)}
+					>
+						Overview
+					</a>
 				</li>
 				{headings
 					.filter(({depth}) => depth > 1 && depth < 4)
 					.map((heading) => (
-						<li
-							className={cx(
-								'hover:text-accent focus:text-accent border-l-2 border-shade-200 py-1 px-2',
-							)}
-						>
-							<a href={`#${heading.slug}`}>{heading.text}</a>
+						<li>
+							<a
+								href={`#${heading.slug}`}
+								className="block px-4 py-2 font-medium border-l-2 hover:text-accent focus:text-accent border-shade-200 ring-inset"
+							>
+								{heading.text}
+							</a>
 						</li>
 					))}
 			</ul>
