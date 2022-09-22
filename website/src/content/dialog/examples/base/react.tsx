711c1b7954352d627844275986b2307b718054c5
Bryan Lee <bryanmylee@gmail.com>
2022-09-22 15:25:27 +1000

    website(refactor): global reset styles

--- website/src/content/dialog/examples/base/react.tsx
+++ website/src/content/dialog/examples/base/react.tsx
@@ -4,13 +4,13 @@ import * as Dialog from '@ally-ui/react-dialog';
 export default function App() {
 	return (
 		<Dialog.Root>
-			<Dialog.Trigger className="px-4 py-2 font-medium text-blue-400 bg-white reset rounded-xl hover:bg-white/80 focus:ring-2 ring-black">
+			<Dialog.Trigger className="px-4 py-2 font-medium text-blue-400 bg-white reset rounded-xl hover:bg-white/80 focus:ring-black">
 				Send feedback
 			</Dialog.Trigger>
 			<Dialog.Portal>
 				<div className="fixed inset-0 flex cursor-default reset flex-center bg-shade/50 backdrop-blur-sm">
 					<Dialog.Content className="w-[65ch] max-w-lg p-8 mx-8 bg-white rounded-xl relative flex flex-col">
-						<Dialog.Title className="mb-2 text-lg font-semibold">
+						<Dialog.Title className="mb-2 text-lg font-semibold text-black">
 							Feedback form
 						</Dialog.Title>
 						<Dialog.Description className="mb-8 text-sm text-gray-400">
@@ -24,7 +24,7 @@ export default function App() {
 								type="text"
 								id="name"
 								placeholder="Bryan Lee"
-								className="w-3/4 p-2 text-sm border border-blue-400 rounded-lg placeholder:text-gray-400 focus:ring-2 ring-black"
+								className="w-3/4 p-2 text-sm border border-blue-400 rounded-lg placeholder:text-gray-400"
 							/>
 						</fieldset>
 						<fieldset className="flex items-start justify-end gap-6 mb-8">
@@ -34,11 +34,11 @@ export default function App() {
 							<textarea
 								id="feedback"
 								placeholder="Enter your feedback..."
-								className="w-3/4 p-2 text-sm border border-blue-400 rounded-lg placeholder:text-gray-400 focus:ring-2 ring-black"
+								className="w-3/4 p-2 text-sm border border-blue-400 rounded-lg placeholder:text-gray-400"
 							/>
 						</fieldset>
-						<Dialog.Close className="self-end px-4 py-2 font-medium text-blue-700 rounded-lg bg-blue-400/25">
-							Send
+						<Dialog.Close className="self-end px-4 py-2 font-medium text-blue-700 rounded-xl bg-blue-400/25">
+							Send feedback
 						</Dialog.Close>
 					</Dialog.Content>
 				</div>
