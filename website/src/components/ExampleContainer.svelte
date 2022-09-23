8341783ff069e81174483afefba137453133de43
Bryan Lee <bryanmylee@gmail.com>
2022-09-23 13:18:08 +1000

    website(feat): direct code examples

---
+++ website/src/components/ExampleContainer.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import {slide} from 'svelte/transition';
+
+	let showCode = false;
+
+	export let code = '';
+	$: codeWithoutTabs = code.replaceAll('\t', '  ');
+</script>
+
+<section
+	class="flex-center relative flex h-52 rounded-xl bg-gradient-to-tr from-[#63d8ff] to-[#3a98f0] text-white"
+>
+	<slot />
+	<button
+		on:click={() => (showCode = !showCode)}
+		class="absolute right-2 bottom-2 rounded-lg px-3 py-2 text-xs hover:bg-white/25 focus:ring-black"
+	>
+		Show code
+	</button>
+</section>
+{#if showCode}
+	<pre transition:slide class="max-h-96"><code>{codeWithoutTabs}</code></pre>
+{/if}
