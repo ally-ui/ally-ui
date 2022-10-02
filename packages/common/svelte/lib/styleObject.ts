4de6c81411e01fa942e841d48865c43bc7d76fb9
Bryan Lee <bryanmylee@gmail.com>
2022-10-02 22:17:26 +1100

    svelte(fix): check if style object needs to be parsed

--- packages/common/svelte/lib/styleObject.ts
+++ packages/common/svelte/lib/styleObject.ts
@@ -1,10 +1,18 @@
-export function styleObject(style: string): Record<string, unknown> {
+export function styleObject(
+	style: Record<string, unknown> | string | null | undefined,
+): Record<string, unknown> {
+	if (style == null) return {};
+	if (typeof style === 'object') return style;
 	return Object.fromEntries(
 		style.split(';').map((kv) => kv.split(':').map((t) => t.trim())),
 	);
 }
 
-export function styleString(style: Record<string, unknown>): string {
+export function styleString(
+	style: Record<string, unknown> | string | null | undefined,
+): string {
+	if (style == null) return '';
+	if (typeof style === 'string') return style;
 	return Object.entries(style)
 		.map(([k, v]) => `${k}:${v}`)
 		.join(';');
