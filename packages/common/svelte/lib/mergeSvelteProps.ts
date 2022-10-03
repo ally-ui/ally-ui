f3f6020b901357c59a91901a077f90006895d1ac
Bryan Lee <bryanmylee@gmail.com>
2022-10-03 13:39:46 +1100

    svelte(feat): merge more than two props

--- packages/common/svelte/lib/mergeSvelteProps.ts
+++ packages/common/svelte/lib/mergeSvelteProps.ts
@@ -2,25 +2,32 @@ import {styleObject, styleString} from './main';
 
 type AnyProps = Record<string, any>;
 
-export function mergeSvelteProps(parentProps: AnyProps, childProps: AnyProps) {
-	// All child props should override.
-	const overrideProps = {...childProps};
+export function mergeSvelteProps(
+	parentProps: AnyProps,
+	...manyChildProps: AnyProps[]
+) {
+	let mergedProps = {...parentProps};
+	for (const childProps of manyChildProps) {
+		// All child props should override.
+		const overrideProps = {...childProps};
 
-	for (const propName in childProps) {
-		const slotPropValue = parentProps[propName];
-		const childPropValue = childProps[propName];
+		for (const propName in childProps) {
+			const mergedPropValue = mergedProps[propName];
+			const childPropValue = childProps[propName];
 
-		if (propName === 'style') {
-			overrideProps[propName] = styleString({
-				...styleObject(slotPropValue),
-				...styleObject(childPropValue),
-			});
-		} else if (propName === 'class') {
-			overrideProps[propName] = [slotPropValue, childPropValue]
-				.filter(Boolean)
-				.join(' ');
+			if (propName === 'style') {
+				overrideProps[propName] = styleString({
+					...styleObject(mergedPropValue),
+					...styleObject(childPropValue),
+				});
+			} else if (propName === 'class') {
+				overrideProps[propName] = [mergedPropValue, childPropValue]
+					.filter(Boolean)
+					.join(' ');
+			}
 		}
-	}
 
-	return {...parentProps, ...overrideProps};
+		Object.assign(mergedProps, overrideProps);
+	}
+	return mergedProps;
 }
--- packages/common/svelte/tests/mergeSvelteProps.test.ts
+++ packages/common/svelte/tests/mergeSvelteProps.test.ts
@@ -20,3 +20,17 @@ it('merges style strings', () => {
 	const merged = mergeSvelteProps(slotProps, childProps);
 	expect(merged.style).toBe('margin:4px;background-color:red;color:white');
 });
+
+it('merges multiple chlid props', () => {
+	const slotProps = {a: 1, b: 2, style: 'margin: 2px; background-color: red'};
+	const childProps = {b: 3, c: 4, style: 'color: white; margin: 4px'};
+	const nextChildProps = {b: 4, d: 5, style: 'color: blue; padding: 2px;'};
+	const merged = mergeSvelteProps(slotProps, childProps, nextChildProps);
+	expect(merged).toStrictEqual({
+		a: 1,
+		b: 4,
+		c: 4,
+		d: 5,
+		style: 'margin:4px;background-color:red;color:blue;padding:2px',
+	});
+});
